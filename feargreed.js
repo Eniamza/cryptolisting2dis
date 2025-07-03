7b4455e8e52c6e7c68b83cfda229207af3bfd6b5
eniamza <eniamza0@gmail.com>
2025-07-03 23:33:47 +0600

    fix: update logo

--- feargreed.js
+++ feargreed.js
@@ -19,7 +19,7 @@ async function fearGreedMonitor() {
         await ax.post(process.env.FEAR_WEBHOOK, {
             embeds: [embed],
             username: "Fear & Greed Index",
-            avatar_url: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=040"
+            avatar_url: "https://cryptologos.cc/logos/bitcoin-btc-logo.png"
 
         }).then(response => {
             console.log("Fear & Greed Index posted successfully:", response.data);
